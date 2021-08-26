983dc66b302fd34fa9b2474dd46904589cbdbce2
Aaron Wei <awei@unc.edu>
2021-08-26 09:21:21 -0400

    FInished Exercise 01

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -4,17 +4,17 @@ __author__ = "730448488"
 lhs = input("Left-hand side: ")
 rhs = input("Right-hand side: ")
 
-lhs = int(lhs)
-rhs = int(rhs)
+intlhs = int(lhs)
+intrhs = int(rhs)
 
-a = lhs < rhs
-print(str(lhs) + " < " + str(rhs) + " is " + str(a))
+a = intlhs < intrhs
+print(lhs + " < " + rhs + " is " + str(a))
 
-b = lhs >= rhs
-print(str(lhs) + " >= " + str(rhs) + " is " + str(b))
+b = intlhs >= intrhs
+print(lhs + " >= " + rhs + " is " + str(b))
 
-c = lhs == rhs
-print(str(lhs) + " == " + str(rhs) + " is " + str(c))
+c = intlhs == intrhs
+print(lhs + " == " + rhs + " is " + str(c))
 
-d = lhs != rhs
-print(str(lhs) + " != " + str(rhs) + " is " + str(d))
+d = intlhs != intrhs
+print(lhs + " != " + rhs + " is " + str(d))
