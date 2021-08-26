dc8f4ce03abc1296736e1296ce3255c79abb5547
Aaron Wei <awei@unc.edu>
2021-08-26 08:37:01 -0400

    Finished Exercise 01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,9 +1,9 @@
 # TODO: Write docstring here
-"""EX01 Hype Machine 8/26/21"""
+"""EX01 Hype Machine 8/26/21."""
 # TODO: Initialize __author__ variable here
 __author__ = "730448488"
 # TODO: Implement your program logic here
 name: str = input("What is your name? ")
 print("Have no fear, " + name + " is here!")
 print(name + "'s the man")
-print("GO FOR THE KNEES " + name.upper())
+print("GO FOR THE KNEES " + name)
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,4 +1,4 @@
-"""EX01 Numeric Operators 8/26/21"""
+"""EX01 Numeric Operators 8/26/21."""
 __author__ = "730448488"
 strx = input("Left-hand side number: ")
 stry = input("Right-hand side number: ")
