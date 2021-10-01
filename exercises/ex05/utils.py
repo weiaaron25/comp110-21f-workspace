75446b9768910d189ac811bdd9fbb99cc58aa9d6
Aaron Wei <awei@unc.edu>
2021-10-01 18:00:40 -0400

    Finished with EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,32 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730448488"
 
 # Define your functions below
+
+
+def only_evens(nums: list[int]) -> list[int]:
+    """Function that returns only the evens in a list of ints."""
+    evens: list[int] = []
+    for i in nums:
+        if i % 2 == 0:
+            evens.append(i)
+    return evens
+
+
+def sub(nums: list[int], start: int, finish: int) -> list[int]:
+    """Function that gives a range in a list of ints."""
+    if start <= 0:
+        start = 1
+    if finish > len(nums):
+        finish = len(nums)
+    new_list: list[int] = []
+    new_list = nums[start - 1:finish - 1]
+    return new_list
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    """Function that combines two lists of ints."""
+    new_list: list[int] = []
+    new_list = list1 + list2
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,53 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730448488"
+
+
+def test_only_evens() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_only_evens2() -> None:
+    ys: list[int] = [12903120947123094, 9283784392039841290, 9128312804739487]
+    assert only_evens(ys) == [12903120947123094, 9283784392039841290]
+
+
+def test_only_evens3() -> None:
+    zs: list[int] = [0, 0, 0, -999]
+    assert only_evens(zs) == [0, 0, 0]
+
+
+def test_sub() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert sub(xs, 0, 3) == [1, 2]
+
+
+def test_sub2() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert sub(xs, 4, 5) == [4]
+
+
+def test_sub3() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert sub(xs, -999, 999) == [1, 2, 3, 4, 5]
+
+
+def test_concat1() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert concat(xs, xs) == xs + xs
+
+
+def test_concat2() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    ys: list[int] = [77, 23]
+    assert concat(xs, ys) == xs + ys
+
+
+def test_concat3() -> None:
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == xs + ys
